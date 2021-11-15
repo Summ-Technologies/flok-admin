fe7ac1b45c14aa4669e5596d42826865273f39c8
Jared Hanson <jred0011@gmail.com>
2021-11-15 00:05:44 -0800

    Update ports to fit existing helm charts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gunicorn server:app -w 2 -b 0.0.0.0:${PORT} --log-config /app/logging.conf ${EXTRA_GUNICORN_ARGS} --daemon
+gunicorn server:app -w 2 -b 0.0.0.0:8001 --log-config /app/logging.conf ${EXTRA_GUNICORN_ARGS} --daemon
 nginx
 tail -f /logs/*
